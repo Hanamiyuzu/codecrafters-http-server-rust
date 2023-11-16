b4c01359e9515ec5ced7642d20b37b12e096acb9
Hanamiyuzu <txw5ifd9@duck.com>
2023-11-16 17:00:26 +0800

    stage respond with content

--- src/main.rs
+++ src/main.rs
@@ -22,19 +22,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     break;
                 }
             }
-            let mut response = "HTTP/1.1 404 Not Found\r\n\r\n";
             let request_str = String::from_utf8_lossy(&buffer);
             println!("Request: {}", request_str);
             let request_lines = request_str.split("\r\n").collect::<Vec<_>>();
             if !request_lines.is_empty() {
                 let start_line = request_lines[0];
                 let start_parts = start_line.split(" ").collect::<Vec<_>>();
-                if start_line.len() >= 2 && start_parts[1] == "/" {
-                    response = "HTTP/1.1 200 OK\r\n\r\n";
+                if start_parts.len() >= 2 && start_parts[1].starts_with("/echo/") {
+                    let response_content = &start_parts[1][6..];
+                    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", response_content.len(), response_content);
+                    println!("Response: {}", response);
+                    socket.write_all(response.as_bytes()).await.unwrap();
                 }
             }
-            println!("Response: {}", response);
-            socket.write_all(response.as_bytes()).await.unwrap();
         });
     }
 }
