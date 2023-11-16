eeeaa1fb04d58b081ea932f4a8bc799f77e869ba
Hanamiyuzu <txw5ifd9@duck.com>
2023-11-16 16:52:55 +0800

    stage respond with 404

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,24 @@ async fn main() -> Result<(), Box<dyn Error>> {
         tokio::spawn(async move {
             println!("Connection from: {}", socket.peer_addr().unwrap());
             let mut buffer = BytesMut::with_capacity(4096);
-            let _ = socket.read_buf(&mut buffer).await.unwrap();
-            let response = "HTTP/1.1 200 OK\r\n\r\n";
+            loop {
+                let bytes_read = socket.read_buf(&mut buffer).await.unwrap();
+                if bytes_read == 0 || buffer.ends_with(b"\r\n\r\n") {
+                    break;
+                }
+            }
+            let mut response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            let request_str = String::from_utf8_lossy(&buffer);
+            println!("Request: {}", request_str);
+            let request_lines = request_str.split("\r\n").collect::<Vec<_>>();
+            if !request_lines.is_empty() {
+                let start_line = request_lines[0];
+                let start_parts = start_line.split(" ").collect::<Vec<_>>();
+                if start_line.len() >= 2 && start_parts[1] == "/" {
+                    response = "HTTP/1.1 200 OK\r\n\r\n";
+                }
+            }
+            println!("Response: {}", response);
             socket.write_all(response.as_bytes()).await.unwrap();
         });
     }
